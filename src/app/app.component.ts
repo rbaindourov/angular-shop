c5217602772a5dc75491702d0d7959622894015c
Robert Baindourov <robert@webdesigner.la>
2021-06-05 23:03:42 -0700

    add config service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Config, ConfigService } from './config';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [ ConfigService ],
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  config: Config | undefined;
+  error: any;
+
+  constructor(private configService: ConfigService) {
+    this.configService.getConfig()
+      .subscribe(
+        (data: Config) => this.config = { ...data }, // success path
+        error => this.error = error // error path
+      );
+  }
   title = 'AStoreForBeauty.com';
 }
